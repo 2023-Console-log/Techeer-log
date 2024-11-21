3efd0286b593f4a20e74cdb78b6b1c7aaab3d28d
김주언 <wndjs803@naver.com>
2024-11-21 23:41:28 +0900

    feat: comment 조회 k6 테스트 스크립트 작성

--- testing/k6/test_script.js
+++ testing/k6/test_script.js
@@ -1,9 +1,25 @@
 import http from 'k6/http';
-import { check, sleep } from 'k6';
+import { check } from 'k6';
 
+export const options = {
+    vus: 1,          // 가상 유저 1명
+    iterations: 10,  // 총 10번 반복
+};
 export default function () {
-    let res = http.get('http://backend1:8080/api/v1/auth/anonymous');
-    check(res, {
-        'status was 200 http://backend1:8080/api/v1/auth/anonymous': (r) => r.status === 200,
+    const BASE_URL = "http://host.docker.internal:8080/api/v1"
+
+    // 익명 토큰 발급
+    let res = http.get(`${BASE_URL}/auth/anonymous`);
+    let authToken = res.headers.Authorization;
+    let headers = {
+        Authorization: authToken, // 추출한 Authorization 값을 사용
+    };
+
+    let testRes = http.get(`${BASE_URL}/comments/1`, {headers:headers});
+    check(testRes, {
+        'comments status was 200': (r) => r.status === 200,
     });
+    const parsedBody = JSON.parse(testRes.body);
+    console.log(parsedBody.data.comments.length);
+
 }
