49ada097849cbdcd41427358a0824c10227befba
Kiminni <imk0980@gmail.com>
2024-12-19 03:16:52 +0900

    refactor: MemberServiceImpl이 아닌 MemberService를 의존하도록 변경

--- backend/src/main/java/com/techeerlog/member/controller/MemberController.java
+++ backend/src/main/java/com/techeerlog/member/controller/MemberController.java
@@ -12,7 +12,7 @@ import com.techeerlog.member.dto.request.SignupRequest;
 import com.techeerlog.member.dto.request.UpdatePasswordRequest;
 import com.techeerlog.member.dto.response.MemberResponse;
 import com.techeerlog.member.dto.response.ProfileResponse;
-import com.techeerlog.member.service.MemberServiceImpl;
+import com.techeerlog.member.service.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -27,14 +27,14 @@ import static com.techeerlog.global.response.ResultCode.*;
 @RequiredArgsConstructor
 public class MemberController {
 
-    private final MemberServiceImpl memberServiceImpl;
+    private final MemberService memberService;
     private final MemberMapper memberMapper;
     private final RefreshTokenService refreshTokenService;
 
     @Operation(summary = "회원가입", description = "회원가입 기능")
     @PostMapping(value = "/signup")
     public ResponseEntity<ResultResponse<MemberResponse>> signUp(@RequestBody SignupRequest signupRequest) {
-        Member member = memberServiceImpl.signUp(signupRequest);
+        Member member = memberService.signUp(signupRequest);
         ResultResponse<MemberResponse> resultResponse = new ResultResponse<>(SIGNUP_SUCCESS, memberMapper.memberToMemberResponse(member));
 
         return ResponseEntity.status(SIGNUP_SUCCESS.getStatus())
@@ -44,7 +44,7 @@ public class MemberController {
     @Operation(summary = "프로필 조회", description = "프로필 조회 기능")
     @GetMapping("/profile")
     public ResponseEntity<ResultResponse<ProfileResponse>> findProfile(@Login AuthInfo authInfo) {
-        ProfileResponse profileResponse = memberServiceImpl.findProfile(authInfo);
+        ProfileResponse profileResponse = memberService.findProfile(authInfo);
         ResultResponse<ProfileResponse> resultResponse = new ResultResponse<>(FIND_PROFILE_SUCCESS, profileResponse);
 
         return ResponseEntity.status(FIND_PROFILE_SUCCESS.getStatus())
@@ -57,7 +57,7 @@ public class MemberController {
                                                            @RequestPart(value = "part", required = false) MultipartFile multipartFile,
                                                            @Login AuthInfo authInfo) {
 
-        memberServiceImpl.editMemberInfo(editMemberRequest, authInfo, multipartFile);
+        memberService.editMemberInfo(editMemberRequest, authInfo, multipartFile);
         refreshTokenService.deleteToken(authInfo.getId());
 
         SimpleResultResponse resultResponse = new SimpleResultResponse(EDIT_PROFILE_SUCCESS);
@@ -70,7 +70,7 @@ public class MemberController {
     @PatchMapping("/password")
     public ResponseEntity<SimpleResultResponse> updatePassword(@RequestBody UpdatePasswordRequest updatePasswordRequest,
                                                                @Login AuthInfo authInfo) {
-        memberServiceImpl.updatePassword(authInfo, updatePasswordRequest);
+        memberService.updatePassword(authInfo, updatePasswordRequest);
         refreshTokenService.deleteToken(authInfo.getId());
         SimpleResultResponse resultResponse = new SimpleResultResponse(UPDATE_CODE_SUCCESS);
 
